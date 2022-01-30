b3962c5410b10f50b7bf25f73c23cf3d5d218701
bigjermreallyreal <87398542+bigjermreallyreal@users.noreply.github.com>
2022-01-30 14:32:05 -0600

    submitting to lab

--- test.js
+++ test.js
@@ -1,1 +1,9 @@
-alert("lol");
+$(document).ready(function(){
+    $("#click").click(function(){
+        if ($("#box").is(":visible")) {
+          $("#box").fadeOut(1000);
+        } else {
+          $("#box").fadeIn(1000);
+        }
+    });
+  });
